2286c30583c156fb41c956acb3e90a27c6dea0a4
OtakuVadER <88473676+OtakuVadER@users.noreply.github.com>
2024-10-15 12:31:46 +0530

    Fix it?

--- OnePace/src/main/kotlin/com/RowdyAvocado/OnePace.kt
+++ OnePace/src/main/kotlin/com/RowdyAvocado/OnePace.kt
@@ -1,66 +1,117 @@
-package com.RowdyAvocado
+package com.OtakuVadER
 
-import com.lagradost.cloudstream3.AnimeSearchResponse
-import com.lagradost.cloudstream3.Episode
-import com.lagradost.cloudstream3.HomePageResponse
-import com.lagradost.cloudstream3.LoadResponse
-import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.MainPageRequest
-import com.lagradost.cloudstream3.SubtitleFile
-import com.lagradost.cloudstream3.TvType
-import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.mainPageOf
-import com.lagradost.cloudstream3.newAnimeSearchResponse
-import com.lagradost.cloudstream3.newHomePageResponse
-import com.lagradost.cloudstream3.newTvSeriesLoadResponse
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.loadExtractor
+import com.lagradost.api.Log
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.AppUtils.parseJson
+import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import org.jsoup.nodes.Element
 
-class OnePace : MainAPI() { // all providers must be an instance of MainAPI
-    override var mainUrl = "https://rentry.org"
-    override var name = "One Pace"
+open class OnepaceProvider : MainAPI() {
+    override var mainUrl = "https://onepace.me"
+    override var name = "OnePace"
     override val hasMainPage = true
     override var lang = "en"
-    override val hasDownloadSupport = true
-    override val supportedTypes =setOf(TvType.Anime)
-    override val mainPage =mainPageOf("/onepace/" to "OnePace")
 
-    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+    override val supportedTypes =
+        setOf(
+            TvType.Anime,
+        )
+
+    override val mainPage =
+        mainPageOf(
+            "/series/one-pace-english-sub/" to "One Pace English Sub",
+            "/series/one-pace-english-dub/" to "One Pace English Dub",
+        )
+
+    override suspend fun getMainPage(
+        page: Int,
+        request: MainPageRequest,
+    ): HomePageResponse {
         val link = "$mainUrl${request.data}"
         val document = app.get(link).document
-        val home = document.select("article div h3").mapNotNull { it.toSearchResult() }
+        val home =
+            document.select("div.seasons.aa-crd > div.seasons-bx").map {
+                it.toSearchResult()
+            }
         return newHomePageResponse(request.name, home)
     }
 
     private fun Element.toSearchResult(): AnimeSearchResponse {
-        val title = this.text()
-        val posterUrl = this.nextElementSibling()?.select("p span img")?.attr("src") ?:""
-        return newAnimeSearchResponse(title, url=title, TvType.Anime) { this.posterUrl = posterUrl }
+        val hreftitle= this.selectFirst("picture img")?.attr("alt")
+        var href=""
+        if (hreftitle!!.isNotEmpty()) {
+            if (hreftitle.contains("Dub")) {
+                href = "https://onepace.me/series/one-pace-english-dub"
+            } else {
+                href = "https://onepace.me/series/one-pace-english-sub"
+            }
+        }
+        val title = this.selectFirst("p")?.text() ?:""
+        val posterUrl = this.selectFirst("img")?.attr("data-src")
+        val dubtype:Boolean
+        val subtype:Boolean
+        if (hreftitle.contains("Dub"))
+        {
+            dubtype = true
+            subtype =false
+        }
+        else
+        {
+            dubtype = false
+            subtype = true
+
+        }
+        return newAnimeSearchResponse(title, Media(href, posterUrl,title).toJson(), TvType.Anime, false) {
+            this.posterUrl = posterUrl
+            addDubStatus(dubExist = dubtype, subExist = subtype)
+        }
+    }
+
+    override suspend fun search(query: String): List<AnimeSearchResponse> {
+        val document = app.get("$mainUrl/?s=$query").document
+        return document.select("ul[data-results] li article").mapNotNull {
+            it.toSearchResult()
+        }
     }
 
     override suspend fun load(url: String): LoadResponse {
-        val title=url.substringAfterLast("/")
-        val document = app.get("https://rentry.org/onepace").document
+
+        val media = parseJson<Media>(url)
+        val document = app.get(media.url).document
+        val ArcINT=media.mediaType?.substringAfter("Arc ")
+        val element= document.selectFirst("div.seasons.aa-crd > div.seasons-bx:contains(S$ArcINT-)")
+        val title = media.mediaType ?:"No Title"
         val poster = "https://images3.alphacoders.com/134/1342304.jpeg"
-        val episodes = mutableListOf<Episode>()
-        val elements= document.selectFirst("article div h3:contains($title)")
-        val description= elements?.nextElementSibling()?.nextElementSibling()?.selectFirst("p")?.text()
-        var PTag = elements
-        repeat(6) {
-            PTag = PTag?.nextElementSibling() // Move to the next sibling 5 times
-        }
-        PTag?.select("div.ntable-wrapper td")?.map { Ep ->
-            val href= Ep.selectFirst("a")?.attr("href") ?:""
-            val episode=Ep.selectFirst("a")?.text()
-            if (href.isNotEmpty())
-            {
-                episodes.add(Episode(href, episode, posterUrl = poster))
+        val plot = document.selectFirst("div.entry-content p")?.text()?.trim()
+            ?: document.selectFirst("meta[name=twitter:description]")?.attr("content")
+        val year = (document.selectFirst("span.year")?.text()?.trim()
+            ?: document.selectFirst("meta[property=og:updated_time]")?.attr("content")
+                ?.substringBefore("-"))?.toIntOrNull()
+        val lst = element?.select("ul.seasons-lst.anm-a li")
+        return if (lst!!.isEmpty()) {
+            newMovieLoadResponse(title, url, TvType.Movie, Media(
+                media.url,
+                mediaType = 1
+            ).toJson()) {
+                this.posterUrl = poster
+                this.plot = plot
+                this.year = year
             }
-        }
-        return newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
-            this.posterUrl = poster
-            this.plot=description
+        } else {
+            @Suppress("NAME_SHADOWING") val episodes = element.select("ul.seasons-lst.anm-a li").mapNotNull {
+                val name = it.selectFirst("h3.title")?.ownText() ?: "null"
+                val href = it.selectFirst("a")?.attr("href") ?: return@mapNotNull null
+                val poster= "https://raw.githubusercontent.com/phisher98/TVVVV/refs/heads/main/OnePack.png"
+                val seasonnumber = it.selectFirst("h3.title > span")?.text().toString().substringAfter("S").substringBefore("-")
+                val season=seasonnumber.toIntOrNull()
+                Episode(Media(href, mediaType = 2).toJson(), name, posterUrl = poster,season = season)
+            }
+            newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
+                this.posterUrl = poster
+                this.plot = plot
+                this.year = year
+            }
         }
     }
 
@@ -68,9 +119,21 @@ class OnePace : MainAPI() { // all providers must be an instance of MainAPI
         data: String,
         isCasting: Boolean,
         subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
+        callback: (ExtractorLink) -> Unit,
     ): Boolean {
-        loadExtractor(data, subtitleCallback, callback)
+        val media = parseJson<Media>(data)
+        val body = app.get(media.url).document.selectFirst("body")?.attr("class") ?: return false
+        val term = Regex("""(?:term|postid)-(\d+)""").find(body)?.groupValues?.get(1) ?: throw ErrorLoadingException("no id found")
+        for (i in 0..4) {
+            val link = app.get("$mainUrl/?trdekho=$i&trid=$term&trtype=${media.mediaType}")
+                .document.selectFirst("iframe")?.attr("src")
+                ?: throw ErrorLoadingException("no iframe found")
+            Log.d("Phisher",link)
+            loadExtractor(link,subtitleCallback, callback)
+        }
         return true
     }
+
+    data class Media(val url: String, val poster: String? = null, val mediaType: String? = null)
+
 }
