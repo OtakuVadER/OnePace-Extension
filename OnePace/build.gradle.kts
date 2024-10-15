ed80ac7a2df8ed53b3617d01d792c38f3d509ec7
OtakuVadER <88473676+OtakuVadER@users.noreply.github.com>
2024-10-15 16:26:02 +0530

    Update build.gradle.kts

--- OnePace/build.gradle.kts
+++ OnePace/build.gradle.kts
@@ -6,7 +6,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
     description = "One Pace"
-    authors = listOf("KillerDogeEmpire, HindiProvider")
+    authors = listOf("OtakuvadER")
 
     /**
      * Status int as the following:
