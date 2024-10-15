74c98d0c9c29c0fdd1d481c6b83f664539b0d2b3
OtakuVadER <88473676+OtakuVadER@users.noreply.github.com>
2024-10-15 22:10:01 +0530

    Update OnePacePlugin.kt

--- OnePace/src/main/kotlin/com/OtakuVadER/OnePacePlugin.kt
+++ OnePace/src/main/kotlin/com/OtakuVadER/OnePacePlugin.kt
@@ -7,7 +7,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class OnePacePlugin : Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(OnePace())
+        registerMainAPI(OnepaceProvider())
         registerExtractorAPI(Streamruby())
         registerExtractorAPI(VidStream())
         registerExtractorAPI(Vidmolynet())
