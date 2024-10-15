22e438548ceb7bcff02a950217ae93560c609d18
OtakuVadER <88473676+OtakuVadER@users.noreply.github.com>
2024-10-15 18:10:00 +0530

    Add Posters For each Arc

---
+++ OnePace/src/main/kotlin/com/OtakuVadER/ArcPosters.kt
@@ -0,0 +1,42 @@
+package com.OtakuVadER
+
+object ArcPosters {
+    val arcPosters = mapOf(
+        1 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-romance-dawn-arc.jpeg.webp",
+        2 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-orange-town-arc.jpeg.webp",
+        3 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-syrup-village-arc.jpeg.webp",
+        4 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-gaimon-arc.jpeg.webp",
+        5 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-baratie-arc.jpeg.webp",
+        6 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-arlong-park-arc.jpeg.webp",
+        7 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-the-adventures-of-buggys-crew-arc.jpeg.webp",
+        8 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-loguetown-arc.jpeg.webp",
+        9 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-reverse-mountain-arc.jpeg.webp",
+        10 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-whisky-peak-arc.jpeg.webp",
+        11 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-the-trials-of-koby-meppo-arc.jpeg.webp",
+        12 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-little-garden-arc.jpeg.webp",
+        13 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-drum-island-arc.jpeg.webp",
+        14 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-arabasta-arc.jpeg.webp",
+        15 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-jaya-arc.jpeg.webp",
+        16 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-skypiea-arc.jpeg.webp",
+        17 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-long-ring-long-land-arc.jpeg.webp",
+        18 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-water-seven-arc.jpeg.webp",
+        19 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-enies-lobby-arc.jpeg.webp",
+        20 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-post-enies-lobby-arc.jpeg.webp",
+        21 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-thriller-bark-arc.jpeg.webp",
+        22 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-sabaody-archipelago-arc.jpeg.webp",
+        23 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-amazon-lily-arc.jpeg.webp",
+        24 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-impel-down-arc.jpeg.webp",
+        25 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-if-you-could-go-anywhere-the-adventures-of-the-straw-hats-arc.jpeg.webp",
+        26 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-marineford-arc.jpeg.webp",
+        27 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-post-war-arc.jpeg.webp",
+        28 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-return-to-sabaody-arc.jpeg.webp",
+        29 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-fishman-island-arc.jpeg.webp",
+        30 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-punk-hazard-arc.jpeg.webp",
+        31 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-dressrosa-arc.jpeg.webp",
+        32 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-zou-arc.jpeg.webp",
+        33 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-whole-cake-island-arc.jpeg.webp",
+        34 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-reverie-arc.jpeg.webp",
+        35 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-wano-arc.jpeg.webp",
+        36 to "https://onepace.me/wp-content/webp-express/webp-images/uploads/2024/09/cover-egghead-arc.jpeg.webp"
+    )
+}
